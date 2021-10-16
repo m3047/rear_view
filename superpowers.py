70982d7e5cefdacdc01b370cb07c0778761d4d1b
Fred Morris <consulting@m3047.net>
2021-10-16 09:06:49 -0700

    Beware of servers sending short TCP packets.

--- superpowers.py
+++ superpowers.py
@@ -86,8 +86,16 @@ class SuperUDPListener(asyncio.DatagramProtocol):
             # the length of the request/response.
             writer.write(len(request).to_bytes(2, byteorder='big')+request)
             await writer.drain()
-            response_length = await reader.read(2)
-            response = await reader.read(int.from_bytes(response_length, byteorder='big'))
+            
+            response_length = int.from_bytes(await reader.read(2), byteorder='big')
+            response = b''
+            while response_length:
+                resp = await reader.read(response_length)
+                if not len(resp):
+                    break
+                response += resp
+                response_length -= len(resp)
+            
             writer.close()
             dns_response = dns.message.from_wire(response)
             if dns_response.rcode() == 0 or not powers() or powers.mode == 'never':
